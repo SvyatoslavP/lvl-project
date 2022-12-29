01a087d1398823a24c5016edaa03b79d1bec5796
Panifidkin S.A <@>
2022-12-29 19:04:17 +0700

    start lvl-project start.spring.io

---
+++ src/test/java/ru/panifidkin/lvlproject/LvlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panifidkin.lvlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
