870238034a1c4373a45beda988a66740e6c57c12
Panifidkin S.A <@>
2024-03-01 00:42:59 +0700

    fix [WARNING] unknown enum constant javax.annotation.meta.When.MAYBE

--- lvl-core/app-core/src/main/java/ru/panifidkin/lvlproject/service/PlayersServiceImpl.java
+++ lvl-core/app-core/src/main/java/ru/panifidkin/lvlproject/service/PlayersServiceImpl.java
@@ -1,11 +1,12 @@
 package ru.panifidkin.lvlproject.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.panifidkin.lvlproject.entity.Player;
 import ru.panifidkin.lvlproject.services.PlayersService;
+
+import javax.annotation.Nullable;
 
 @Slf4j
 @Service
