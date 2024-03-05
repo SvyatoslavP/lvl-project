2fcbb5e4561d99baab3584cead494a6f237971f3
Panifidkin S.A <@>
2024-03-05 19:16:13 +0700

    add tests

---
+++ lvl-test/integration-test/src/test/java/ru/panifidkin/lvlproject/TestTest.java
@@ -0,0 +1,15 @@
+package ru.panifidkin.lvlproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestTest {
+    @Test
+    void shouldCallServicesAndConvertersUponHasPlayerId() {
+        //given
+        //when
+        //then
+        assertTrue(true);
+    }
+}
