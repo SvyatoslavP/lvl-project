33053a9fd7e8c6e58e375d3427aa63f4891704c5
Panifidkin S.A <@>
2024-03-05 22:23:18 +0700

    add integration test

---
+++ lvl-test/integration-test/src/test/java/ru/panifidkin/lvlproject/FrontendPlayerServiceIT.java
@@ -0,0 +1,55 @@
+package ru.panifidkin.lvlproject;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Rollback;
+import ru.panifidkin.lvlproject.annotation.IT;
+import ru.panifidkin.lvlproject.dao.PlayerRepository;
+import ru.panifidkin.lvlproject.entity.Player;
+import ru.panifidkin.lvlproject.services.FrontendPlayerService;
+
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IT
+@Transactional
+@Rollback
+@RequiredArgsConstructor
+class FrontendPlayerServiceIT {
+
+    private final FrontendPlayerService service;
+    private final PlayerRepository repository;
+
+    @Test
+    void shouldFindPlayerById() {
+        //given
+        final Player player = Player.builder()
+                .firstName("testFirstName")
+                .lastName("testLastName")
+                .birthDate(LocalDate.of(1999, 1, 1))
+                .build();
+        final String rqBody = """
+                {
+                    "playerId" : "%s"
+                }
+                """.formatted(player.getPersistenceId());
+
+        repository.saveOrUpdate(player);
+        //when
+        final String rsBody = service.findPlayerById(rqBody);
+        //then
+        assertThat(rsBody).isEqualTo("""
+                {
+                  "player": {
+                    "firstName": "testFirstName",
+                    "lastName": "testLastName",
+                    "birthDate": "1999-01-01"
+                  },
+                  "status": {
+                    "statusCode": "0"
+                  }
+                }""");
+    }
+}
